32f8ab4ffda3341250619388503283ab299e13fb
241105 <241105@std.hi-joho.ac.jp>
2025-06-16 12:13:39 +0900

    chapter11

---
+++ src/kadai1/Character.java
@@ -0,0 +1,10 @@
+package kadai1;
+
+public abstract class Character {
+    int hp;
+    String name;
+    public abstract void attack(Matango hp);
+    public void run(){
+        System.out.println(this.name+"は逃げ出した");
+    }
+}
